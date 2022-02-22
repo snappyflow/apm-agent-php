8d0cfc886cb1546a8a37901c18d05e8fc861351e
pradeep-dot-jaiswar <pradeep.jaiswar@maplelabs.com>
2022-02-22 15:32:54 +0530

    Support Automation build

---
+++ sf_php_build.sh
@@ -0,0 +1,21 @@
+PHP_VERSION=$1
+PHP_AGENT_DIR=$(pwd)
+EXTENSION_DIR="${PHP_AGENT_DIR}/extensions"
+EXTENSION_CFG_DIR="${PHP_AGENT_DIR}/etc"
+BOOTSTRAP_FILE_PATH="${PHP_AGENT_DIR}/src/bootstrap_php_part.php"
+BACKUP_EXTENSION=".agent.bck"
+
+
+rm -rf ${EXTENSION_CFG_DIR}
+rm -rf ${EXTENSION_DIR}
+
+mkdir -p ${EXTENSION_CFG_DIR}
+mkdir -p ${EXTENSION_DIR}
+
+cp -rf src/ext/modules/* ${EXTENSION_DIR}
+
+mkdir output
+filename="sf-apm-php-agent-${PHP_VERSION}.tar.gz"
+
+
+tar cvzf output/${filename} ./src/*
